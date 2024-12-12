68bdc8671a052bf4f8936795ed02835d80365ca6
andrewdoro <elfandreis@gmail.com>
2024-12-12 13:46:21 +0200

    fix: expose types useSearch

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "@/components";
 export { useSelectionDimensions } from "@/hooks/useSelectionDimensions";
 export { usePDFJump } from "@/lib/pages";
 export * from "@/lib/internal";
-export { useSearch } from "@/components/Search/useSearch";
+export * from "@/components/Search/useSearch";
 export * from "@/components/Search/useSearchPosition";
