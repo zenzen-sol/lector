e2b8fe62e3185297bb9b5ed49dc00e9cdcd4f2b8
andrewdoro <elfandreis@gmail.com>
2024-12-12 13:45:32 +0200

    fix: expose search position utils

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { useSelectionDimensions } from "@/hooks/useSelectionDimensions";
 export { usePDFJump } from "@/lib/pages";
 export * from "@/lib/internal";
 export { useSearch } from "@/components/Search/useSearch";
+export * from "@/components/Search/useSearchPosition";
