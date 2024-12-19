7631968cb079996b50562ad32e4b75257847dc95
andrewdoro <elfandreis@gmail.com>
2024-12-19 15:31:03 +0200

    fix: add prepack

---
+++ packages/lector/scripts/prepack.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Place ourselves in the package directory
+cd "$(dirname "$0")/.."
+
+# Copy the README & License from the root of the repository
+cp -f ../../README.md ../../LICENSE ./
+
+# Read the version from package.json
+VERSION=$(cat package.json | jq -r '.version')
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  # macOS requires an empty string as the backup extension
+  sed -i '' "s/0.0.0-inject-version-here/${VERSION}/g" dist/index.js
+else
+  # Ubuntu (CI/CD) doesn't
+  sed -i "s/0.0.0-inject-version-here/${VERSION}/g" dist/index.js
+fi
